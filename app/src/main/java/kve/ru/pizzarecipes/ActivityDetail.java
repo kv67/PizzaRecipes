4f59abe9209fb23fc8434bdf9a5ab33782ea5360
kv67 <kv67@narod.ru>
2020-03-01 15:38:22 +0300

    ActivityDetail added
    Signed-off-by: kv67 <kv67@narod.ru>

---
+++ app/src/main/java/kve/ru/pizzarecipes/ActivityDetail.java
@@ -0,0 +1,14 @@
+package kve.ru.pizzarecipes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityDetail extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_detail);
+  }
+}
